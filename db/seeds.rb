7e6cdec9597ea2f92578cd63799e08929e324469
Rose Lutz <rose.lutz.85@gmail.com>
2018-04-03 16:42:47 +0200

    Adding seeds (launch a bundle install, since two new gems in the gemfile)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,75 +1,143 @@
 puts 'Cleaning all tables of the database...'
-User.destroy_all
-Costume.destroy_all
 Booking.destroy_all
+Costume.destroy_all
+User.destroy_all
 
 
 puts 'Creating users'
 
-john = User.create!(
+bob = User.create!(
   email: "bob.lean@test.com",
-  encrypted_password: "123456",
+  password: "123456",
+  first_name: "Robert",
+  last_name: "Lean",
+  avatar: File.open(Rails.root.join("db/fixtures/users/bob.jpg")),
+  city: "Nantes",
+  phone: "0203040506"
+  )
+
+felindra = User.create!(
+  email: "felindra.boyard@test.com",
+  password: "azerty",
+  first_name: "Felindra",
+  last_name: "Boyard",
+  avatar: File.open(Rails.root.join("db/fixtures/users/felindra.jpg")),
+  city: "Paris",
+  phone: "0103040506"
+  )
+
+john = User.create!(
+  email: "john.attan@test.com",
+  password: "123456",
+  first_name: "John",
+  last_name: "Attan",
+  avatar: File.open(Rails.root.join("db/fixtures/users/john.jpg")),
+  city: "Lyon",
+  phone: "0403040506"
   )
 
 
+puts 'Creating costumes for owners'
 
-puts 'Creating costumes'
-banana = Costume.create!(name: "banana")
-orange = Costume.create!(name: "orange")
-rhum = Costume.create!(name: "rhum")
-vodka = Costume.create!(name: "vodka")
-
-
-banana_moon = Booking.create!(name: "banana moon")
-mechanical_orange = Booking.create!(name: "mechanical orange")
-all_roads_lead_to_rhum = Booking.create!(name: "all roads lead to rhum")
-vodkoka = Booking.create!(name: "vodkoka")
-
-
-
-b_dose_1 = Dose.create!(
-  cocktail: banana_moon,
-  ingredient: banana,
-  description: "10cl"
+ladybug = Costume.create!(
+  name: "Wonderfull ladybug !!!",
+  price: 10,
+  description: "A Wonderfull costume, really easy to wear. One antena is off.",
+  size: "3",
+  category: "animals",
+  image: File.open(Rails.root.join("db/fixtures/costumes/ladybug.jpg")),
+  city: "Nantes",
+  availability: true,
+  user: john
   )
 
-b_dose_2 = Dose.create!(
-  cocktail: banana_moon,
-  ingredient: rhum,
-  description: "4cl"
+pirate = Costume.create!(
+  name: "pirate",
+  price: 35,
+  description: "Be the queen of the seas thanks to this costume. Very good state of conservation.",
+  size: "S",
+  category: "celebrations",
+  image: File.open(Rails.root.join("db/fixtures/costumes/pirate.jpg")),
+  city: "Paris",
+  availability: false,
+  user: john
   )
 
-m_dose_1 = Dose.create!(
-  cocktail: mechanical_orange,
-  ingredient: orange,
-  description: "10cl"
-  )
-m_dose_2 = Dose.create!(
-  cocktail: mechanical_orange,
-  ingredient: vodka,
-  description: "3cl"
-  )
-
-a_dose_1 = Dose.create!(
-  cocktail: all_roads_lead_to_rhum,
-  ingredient: orange,
-  description: "13cl"
-  )
-a_dose_2 = Dose.create!(
-  cocktail: all_roads_lead_to_rhum,
-  ingredient: rhum,
-  description: "3cl")
-
-v_dose_1 = Dose.create!(
-  cocktail: vodkoka,
-  ingredient: banana,
-  description: "20cl"
-  )
-v_dose_2 = Dose.create!(
-  cocktail: vodkoka,
-  ingredient: vodka,
-  description: "5cl"
+unicorn = Costume.create!(
+  name: "unicorn (hey hey)",
+  price: 59,
+  description: "Ride the unicorn...",
+  size: "XXL",
+  category: "animals",
+  image: File.open(Rails.root.join("db/fixtures/costumes/unicorn.jpg")),
+  city: "Lyon",
+  availability: true,
+  user: john
   )
 
+jasmine = Costume.create!(
+  name: "Jasmine in blue",
+  price: 12,
+  description: "Have a blue dream with Aladdin. Old costume, but not too much wholes.",
+  size: "8",
+  category: "cartoon",
+  image: File.open(Rails.root.join("db/fixtures/costumes/jasmine.jpg")),
+  city: "Marseille",
+  availability: true,
+  user: felindra
+  )
+
+
+puts 'Creating bookings for users'
+
+booking1 = Booking.create!(
+  user: bob,
+  costume: unicorn,
+  start_on: "2018-04-25",
+  end_on: "2018-04-26",
+  status: "accepted",
+  price: 49,
+  message: "Hey! Thanks! :)"
+  )
+
+booking2 = Booking.create!(
+  user: bob,
+  costume: jasmine,
+  start_on: "2018-08-03",
+  end_on: "2018-08-13",
+  status: "pending",
+  price: 10,
+  message: "Do you have this costume also in another size?"
+  )
+
+booking3 = Booking.create!(
+  user: felindra,
+  costume: pirate,
+  start_on: "2018-08-03",
+  end_on: "2018-08-13",
+  status: "pending",
+  price: 35,
+  message: ""
+  )
+
+booking4 = Booking.create!(
+  user: felindra,
+  costume: ladybug,
+  start_on: "2018-12-13",
+  end_on: "2018-12-13",
+  status: "accepted",
+  price: 10,
+  message: "For my little girl. This costume is sooo cute!"
+  )
+
+booking5 = Booking.create!(
+  user: bob,
+  costume: pirate,
+  start_on: "2018-08-03",
+  end_on: "2018-08-13",
+  status: "refused",
+  price: 35,
+  message: "This costume seems to be rented, but just in case..."
+  )
 
 puts 'Finished...'
