9f218c107d2c80023cae9f352b4ab1652f3101cf
benoit mathonat <benoit.mathonat@gmail.com>
2018-04-03 19:38:07 +0200

    ajout de la fonction new costume, manque la show

--- app/controllers/owner/costumes_controller.rb
+++ app/controllers/owner/costumes_controller.rb
@@ -1,19 +1,28 @@
 class Owner::CostumesController < ApplicationController
-  COSTUMES_PARAMS =
+
   def new
-    costume = Costume.new
+    @costume = Costume.new
   end
 
   def create
     @costume = Costume.new(costume_params)
     @costume.save
 
-    redirect_to owner_costume_path(@costume)
+    redirect_to costume_path(@costume)
   end
 
   private
 
   def costume_params
-    params.require(:costume).permit(:name, :price, :description, :size, :category, :image, :city, :availability)
+    params.require(:costume).permit(
+      :name,
+      :price,
+      :description,
+      :size,
+      :category,
+      :image,
+      :city,
+      :availability
+    )
   end
 end
