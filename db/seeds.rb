f581b97e55f9935d6ce52226eb8e663fcdb389ca
Rose Lutz <rose.lutz.85@gmail.com>
2018-04-03 12:37:41 +0200

    Update db with user's profile fields (first_name, last_name, etc.)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,75 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning all tables of the database...'
+User.destroy_all
+Costume.destroy_all
+Booking.destroy_all
+
+
+puts 'Creating users'
+
+john = User.create!(
+  email: "bob.lean@test.com",
+  encrypted_password: "123456",
+  )
+
+
+
+puts 'Creating costumes'
+banana = Costume.create!(name: "banana")
+orange = Costume.create!(name: "orange")
+rhum = Costume.create!(name: "rhum")
+vodka = Costume.create!(name: "vodka")
+
+
+banana_moon = Booking.create!(name: "banana moon")
+mechanical_orange = Booking.create!(name: "mechanical orange")
+all_roads_lead_to_rhum = Booking.create!(name: "all roads lead to rhum")
+vodkoka = Booking.create!(name: "vodkoka")
+
+
+
+b_dose_1 = Dose.create!(
+  cocktail: banana_moon,
+  ingredient: banana,
+  description: "10cl"
+  )
+
+b_dose_2 = Dose.create!(
+  cocktail: banana_moon,
+  ingredient: rhum,
+  description: "4cl"
+  )
+
+m_dose_1 = Dose.create!(
+  cocktail: mechanical_orange,
+  ingredient: orange,
+  description: "10cl"
+  )
+m_dose_2 = Dose.create!(
+  cocktail: mechanical_orange,
+  ingredient: vodka,
+  description: "3cl"
+  )
+
+a_dose_1 = Dose.create!(
+  cocktail: all_roads_lead_to_rhum,
+  ingredient: orange,
+  description: "13cl"
+  )
+a_dose_2 = Dose.create!(
+  cocktail: all_roads_lead_to_rhum,
+  ingredient: rhum,
+  description: "3cl")
+
+v_dose_1 = Dose.create!(
+  cocktail: vodkoka,
+  ingredient: banana,
+  description: "20cl"
+  )
+v_dose_2 = Dose.create!(
+  cocktail: vodkoka,
+  ingredient: vodka,
+  description: "5cl"
+  )
+
+
+puts 'Finished...'
